8c39bec47384658ff22395f04233b469be39fe68
ms03831 <ms03831@st.habib.edu.pk>
2019-12-02 12:17:43 +0500

    start up projct

---
+++ Humraah/Humraah/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Humraah.Startup))]
+namespace Humraah
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
