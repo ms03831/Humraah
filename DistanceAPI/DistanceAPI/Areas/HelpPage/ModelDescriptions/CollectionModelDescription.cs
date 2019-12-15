7c9939733fb34d2d6b68a11c5d2072bb982d535d
ms03831 <ms03831@st.habib.edu.pk>
2019-12-15 18:38:38 +0500

    distance api

---
+++ DistanceAPI/DistanceAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace DistanceAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
