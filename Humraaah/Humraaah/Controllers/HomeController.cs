b98bd3de7d984843512662895e5c49634e85ba63
ms03831 <ms03831@st.habib.edu.pk>
2019-12-22 15:58:33 +0500

    changes made

--- Humraaah/Humraaah/Controllers/HomeController.cs
+++ Humraaah/Humraaah/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Humraaah.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +11,26 @@ namespace Humraaah.Controllers
 {
     public class HomeController : Controller
     {
+        private HumraahContext db = new HumraahContext();
+
         public ActionResult Index()
         {
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            if (user != null)
+            {
+                string strCurrentUserId = user.Id;
+                User_ s = db.User_.Find(strCurrentUserId);
+                if (s != null)
+                {
+                    ViewBag.Role = 1;
+                }
+                else
+                {
+                    Station s1 = db.Stations.Find(strCurrentUserId);
+                    if (s1 != null)
+                        ViewBag.Role = 2;
+                }
+            }
             return View();
         }
 
